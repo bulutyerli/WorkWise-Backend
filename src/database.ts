4398aba5898a6edbaa5cb443914595ea45cd42ff
bulut yerli <bulut@buluts-Mac-mini.local>
2024-06-02 19:47:13 +0300

    vercel

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,4 @@
-import { Pool } from 'pg';
-import { Kysely, PostgresDialect } from 'kysely';
 import { CompanyDatabase } from './types/types';
+import { createKysely } from '@vercel/postgres-kysely';
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    max: 1,
-  }),
-});
-
-export const db = new Kysely<CompanyDatabase>({
-  dialect,
-});
+export const db = createKysely<CompanyDatabase>();
