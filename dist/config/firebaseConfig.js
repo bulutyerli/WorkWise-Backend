d85e86653f0d828e43e1c64194c4a7350dba3de5
bulut yerli <bulut@buluts-Mac-mini.local>
2024-06-03 14:40:34 +0300

    firebase vercel fix v2

--- dist/config/firebaseConfig.js
+++ dist/config/firebaseConfig.js
@@ -1,23 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.auth = exports.firebase = exports.getGCPCredentials = void 0;
+exports.auth = exports.firebase = void 0;
 const app_1 = require("firebase-admin/app");
 const auth_1 = require("firebase-admin/auth");
-const getGCPCredentials = () => {
-    // for Vercel, use environment variables
-    return process.env.GCP_PRIVATE_KEY
-        ? {
-            credentials: {
-                client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GCP_PRIVATE_KEY,
-            },
-            projectId: process.env.GCP_PROJECT_ID,
-        }
-        : // for local development, use gcloud CLI
-            {};
-};
-exports.getGCPCredentials = getGCPCredentials;
 exports.firebase = (0, app_1.initializeApp)({
-    credential: (0, app_1.cert)((0, exports.getGCPCredentials)()),
+    credential: (0, app_1.cert)({
+        projectId: process.env.GCP_PROJECT_ID,
+        clientEmail: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+        privateKey: process.env.GCP_PRIVATE_KEY,
+    }),
 });
 exports.auth = (0, auth_1.getAuth)(exports.firebase);
