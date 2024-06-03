58dae3ce67696de3b28cd2cdd2a37dc1a4991fce
bulut yerli <bulut@buluts-Mac-mini.local>
2024-06-03 05:32:05 +0300

    fix firebase admin sdk

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,8 +1,12 @@
-import { initializeApp, App, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, App, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS as string
+);
+
 export const firebase: App = initializeApp({
-  credential: applicationDefault(),
+  credential: cert(serviceAccount),
 });
 
 export const auth = getAuth(firebase);
